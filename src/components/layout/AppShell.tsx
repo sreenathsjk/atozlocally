fb882bd3eace0e457836e6bbf2e2e5fd607ae4cc
sreenathsjk <content2u.sj@gmail.com>
2026-03-25 15:11:13 +0530

    Update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,14 +1,7 @@
-import BottomNav from "@/components/layout/BottomNav";
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+export default function AppShell({ children }: any) {
   return (
-    <div className="relative min-h-dvh bg-dark-900 noise-bg">
-      {/* Background orbs */}
-      <div className="orb w-96 h-96 bg-neon-cyan opacity-[0.04] -top-20 -right-20 pointer-events-none" />
-      <div className="orb w-80 h-80 bg-neon-purple opacity-[0.04] bottom-40 -left-20 pointer-events-none" />
-
-      <main className="relative z-10 max-w-lg mx-auto pb-nav">{children}</main>
-      <BottomNav />
+    <div style={{ padding: "16px" }}>
+      {children}
     </div>
   );
 }
